defc3aee89933ac6447ce540e7b391bcc7b718a3
Alisherov017 <r.alisherov2005@gmail.com>
2024-04-18 11:53:13 +0600

    first commit

--- JS/main.js
+++ JS/main.js
@@ -55,8 +55,7 @@ async function getQuery(endpoint) {
 }
 
 //! register
-
-registerBtn.addEventListener("click", (e) => {
+registerBtn.addEventListener("click", (e) => {                
   e.preventDefault();
   overlay.style.display = "block";
   form.style.display = "block";
@@ -89,202 +88,200 @@ form.addEventListener("submit", async (event) => {
   await registration();
 });
 
-//! function registartion
+  //! function registartion
 
-async function registration() {
-  if (passwordInp.value.length < 8) {
-    console.error("Password must be more than 8 characters!");
-    return;
-  }
+  async function registration() {
+    if (passwordInp.value.length < 8) {
+      console.error("Password must be more than 8 characters!");
+      return;
+    }
 
-  if (passwordInp.value !== confirmInp.value) {
-    console.error("Password and its confirmation don't match!");
-    return;
-  }
+    if (passwordInp.value !== confirmInp.value) {
+      console.error("Password and its confirmation don't match!");
+      return;
+    }
 
-  let users = await getUsers();
+    let users = await getUsers();
 
-  if (users.some((item) => item.email === emailInp.value)) {
-    alert("This email is already taken!");
-    return;
-  }
+    if (users.some((item) => item.email === emailInp.value)) {
+      alert("This email is already taken!");
+      return;
+    }
 
-  let data = {
-    username: usernameInp.value,
-    email: emailInp.value,
-    password: passwordInp.value,
-  };
+    let data = {  
+      username: usernameInp.value,
+      email: emailInp.value,
+      password: passwordInp.value,
+    };
 
-  console.log(data);
+    console.log(data);
 
-  try {
-    await fetch("http://localhost:8000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+      await fetch("http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    } catch (error) {
+      console.error(error);
+    }
 
-  form.reset();
-  closeModal();
-}
-
-//! function login
-
-async function getUsers() {
-  // let res = await fetch("http://localhost:8000/users");
-  // let data = await res.json();
-  const data = await getQuery("users");
-  console.log(data);
-  return data;
-}
-
-loginForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  await login();
-  closeModal();
-});
-
-async function login() {
-  if (!emailLoginInp.value.trim() || !passwordLoginInp.value.trim()) {
-    alert("Some inputs are empty!");
-    return;
-  }
-
-  let users = await getUsers();
-
-  const foundUser = users.find((user) => user.email === emailLoginInp.value);
-
-  if (!foundUser) {
-    alert("User not found!");
-    return;
-  }
-
-  if (foundUser.password !== passwordLoginInp.value) {
-    alert("Wrong password!");
-    return;
-  }
-
-  localStorage.setItem(
-    "user",
-    JSON.stringify({ username: foundUser.username, email: foundUser.email })
-  );
-  getName();
-  checkIsAdmin();
-  render();
-  loginForm.reset();
-}
-
-//! check admin
-
-function checkIsAdmin() {
-  const email = JSON.parse(localStorage.getItem("user"))?.email;
-  if (email !== "admin@gmail.com") {
-    addBtn.style.display = "none";
-    return false;
-  } else {
-    addBtn.style.display = "block";
-    return true;
-  }
-}
-checkIsAdmin();
-
-//! function add products to db
-
-addBtn.addEventListener("click", () => {
-  addForm.style.display = "block";
-  overlay.style.display = "block";
-});
-
-addForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-
-//! function get products from db
-addProductBtn.addEventListener("click", async () => {
-  if (
-    !imgInp.value ||
-    !titleInp.value ||
-    !priceInp.value ||
-    !categoryInp.value ||
-    !descriptionArea.value
-  ) {
-    alert("Some inputs are empty!");
-    return;
-  }
-  const newProduct = {
-    image: imgInp.value,
-    title: titleInp.value,
-    price: priceInp.value,
-    category: categoryInp.value,
-    description: descriptionArea.value,
-  };
-
-  try {
-    const response = await fetch("http://localhost:8000/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newProduct),
-    });
-    console.log(response);
-    imgInp.value = "";
-    titleInp.value = "";
-    priceInp.value = "";
-    categoryInp.value = "";
-    descriptionArea.value = "";
+    form.reset();
     closeModal();
-    render(newProduct);
-  } catch (error) {}
-});
+  }
 
-//! function render
+  //! function login
 
-let search = "";
-let category = "";
-let page = 1;
-const limit = 2;
+  async function getUsers() {
+    const data = await getQuery("users");
+    console.log(data);
+    return data;
+  }
 
-async function render() {
-  let API = category
-    ? `${PRODUCTS_API}?q=${search}&category=${category}&_page=${page}&_limit=${limit}`
-    : `${PRODUCTS_API}?q=${search}&_page=${page}&_limit=${limit}`;
-  const res = await fetch(API);
-  const data = await res.json();
-
-  container.innerHTML = "";
-
-  data.forEach((product) => {
-    container.innerHTML += `
-      <div class="producCard">
-    <div class="card" style="width: 18rem;">
-      <img  src=${product.image} class="card-img-top" alt="Product image">
-      <div class="card-body">
-        <h2 class="card-title">${product.title}</h2>
-        <h5 class="card-price">${product.price}$</h5>
-        <span class="card-category">${product.category}</span>
-        <p class="card-text">${product.description}</p>
-         ${
-           checkIsAdmin()
-             ? ` <div class="buttons">
-               <button id=${product.id} class="edit-btn">Edit</button>
-               <button id=${product.id} class="delete-btn">Delete</button>
-             </div>`
-             : ""
-         }
-      </div>
-    </div>
-    </div>
-    `;
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    await login();
+    closeModal();
   });
-}
 
-render();
+  async function login() {
+    if (!emailLoginInp.value.trim() || !passwordLoginInp.value.trim()) {
+      alert("Some inputs are empty!");
+      return;
+    }
+
+    let users = await getUsers();
+
+    const foundUser = users.find((user) => user.email === emailLoginInp.value);
+
+    if (!foundUser) {
+      alert("User not found!");
+      return;
+    }
+
+    if (foundUser.password !== passwordLoginInp.value) {
+      alert("Wrong password!");
+      return;
+    }
+
+    localStorage.setItem(
+      "user",
+      JSON.stringify({ username: foundUser.username, email: foundUser.email })
+    );
+    getName();
+    checkIsAdmin();
+    render();
+    loginForm.reset();
+  }
+
+  //! check admin
+
+  function checkIsAdmin() {
+    const email = JSON.parse(localStorage.getItem("user"))?.email;
+    if (email !== "admin@gmail.com") {
+      addBtn.style.display = "none";
+      return false;
+    } else {
+      addBtn.style.display = "block";
+      return true;
+    }
+  }
+  checkIsAdmin();
+
+  //! function add products to db
+
+  addBtn.addEventListener("click", () => {
+    addForm.style.display = "block";
+    overlay.style.display = "block";
+  });
+
+  addForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
+  //! function get products from db
+  addProductBtn.addEventListener("click", async () => {
+    if (
+      !imgInp.value ||
+      !titleInp.value ||
+      !priceInp.value ||
+      !categoryInp.value ||
+      !descriptionArea.value
+    ) {
+      alert("Some inputs are empty!");
+      return;
+    }
+    const newProduct = {
+      image: imgInp.value,
+      title: titleInp.value,
+      price: priceInp.value,
+      category: categoryInp.value,
+      description: descriptionArea.value,
+    };
+
+    try {
+      const response = await fetch("http://localhost:8000/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newProduct),
+      });
+      console.log(response);
+      imgInp.value = "";
+      titleInp.value = "";
+      priceInp.value = "";
+      categoryInp.value = "";
+      descriptionArea.value = "";
+      closeModal();
+      render(newProduct);
+    } catch (error) {}
+  });
+
+  //! function render
+
+  let search = "";
+  let category = "";
+  let page = 1;
+  const limit = 2;
+
+  async function render() {
+    let API = category
+      ? `${PRODUCTS_API}?q=${search}&category=${category}&_page=${page}&_limit=${limit}`
+      : `${PRODUCTS_API}?q=${search}&_page=${page}&_limit=${limit}`;
+    const res = await fetch(API);
+    const data = await res.json();
+
+    container.innerHTML = "";
+
+    data.forEach((product) => {
+      container.innerHTML += `
+        <div class="producCard">
+      <div class="card" style="width: 18rem;">
+        <img  src=${product.image} class="card-img-top" alt="Product image">
+        <div class="card-body">
+          <h2 class="card-title">${product.title}</h2>
+          <h5 class="card-price">${product.price}$</h5>
+          <span class="card-category">${product.category}</span>
+          <p class="card-text">${product.description}</p>
+          ${
+            checkIsAdmin()
+              ? ` <div class="buttons">
+                <button id=${product.id} class="edit-btn">Edit</button>
+                <button id=${product.id} class="delete-btn">Delete</button>
+              </div>`
+              : ""
+          }
+        </div>
+      </div>
+      </div>
+      `;
+    });
+  }
+
+  render();
 
 //! function see name
 
